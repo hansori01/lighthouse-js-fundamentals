5431ac93cb5f69ef2f71c69d7b20af78374f6e8b
hansori01 <19sorihan88@gmail.com>
2021-01-14 18:10:25 +0000

    made some changes

--- Katas/Kata_5_PercentEncodedString.js
+++ Katas/Kata_5_PercentEncodedString.js
@@ -32,17 +32,25 @@ console.log(urlEncode("blue is greener than purple for sure"));
 
 
 /* This for some reason isn't working.. 
-the for...of loop I thought in my mind should be working */
+the for...of loop I thought in my mind should be working 
+Fred Lavoie helped - the 'elm' variable only lives as long as the individual loops.
+So after the loop runs the %20 isn't accessible outside of that loop scope.
+*/
 
 const urlEncode = text => {
   let array = text.split('');
+  //because for...of loop's iterator variable only exists within the loop scope, 
+  // store the new string into a new variable.
+  let url = '';
   for (let elm of array) {
     switch (elm) {
       case ' ':
       elm = '%20';
     }
+    //within the loop, log the characters and the newly transformed character. 
+    url += elm
   }
-  return array.join('');
+  return url;
 }
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode(" Lighthouse Labs "));
