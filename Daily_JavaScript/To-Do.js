8bc8baa8d0f84b4f5e32ccc929e2b645f587b195
hansori01 <19sorihan88@gmail.com>
2021-01-15 05:01:16 +0000

    set up todos to brush on up on older challenges

---
+++ Daily_JavaScript/To-Do.js
@@ -0,0 +1,58 @@
+/*
+
+Store exercises I want to do for my daily JS Practice.
+Comment out if done.
+
+*/
+
+// https://web.compass.lighthouselabs.ca/prep/prep/activities/646
+// turn strings into camelCasing strings.
+
+const camelCase = function (input) {
+
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
+
+
+//Kata Stretch 7 - https://web.compass.lighthouselabs.ca/activities/646
+// [a,b] - repeat 'a' as many time as 'b'
+
+const repeatNumbers = function (data) {
+
+  return ___;
+}
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+
+
+// Kata 5 
+// Turn into url -> any space ' ' gets turned into %20
+
+const urlEncode = text => {
+
+}
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
+
+
+//Kata 4
+//return an object of the person with the longest name
+// returns:  { name: "Jeremiah", course: "Web" }
+
+console.log(instructorWithLongestName([
+  { name: "Samuel", course: "iOS" },
+  { name: "Jeremiah", course: "Web" },
+  { name: "Ophilia", course: "Web" },
+  { name: "Donald", course: "Web" }
+]));
+console.log(instructorWithLongestName([
+  { name: "Matthew", course: "Web" },
+  { name: "David", course: "iOS" },
+  { name: "Domascus", course: "Web" }
+]));
