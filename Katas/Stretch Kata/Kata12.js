f89ef0132af83b3ee4e9ede1028af71cdaab67e3
hansori01 <19sorihan88@gmail.com>
2021-01-19 23:37:22 +0000

    kata 12 almost solved. waiting for answer from forum

---
+++ Katas/Stretch Kata/Kata12.js
@@ -0,0 +1,66 @@
+/* create a single object to organize the instructors based on their course.
+
+*/
+
+
+const organizeInstructors = function (instructors) {
+  let classGuide = {};
+  for (let subject of instructors) {
+    if (subject.course === 'iOS') {
+      classGuide.iOS = [];
+      classGuide.iOS.push(subject.name)
+    } if (subject.course === 'Web') {
+      classGuide.Web = [];
+      classGuide.Web.push(subject.name)
+    } if (subject.course === 'Blockchain') {
+      classGuide.Blockchain = [];
+      classGuide.Blockchain.push(subject.name)
+    }
+  }
+
+  return classGuide;
+}
+
+console.log(organizeInstructors([
+  { name: "Samuel", course: "iOS" },
+  { name: "Victoria", course: "Web" },
+  { name: "Karim", course: "Web" },
+  { name: "Donald", course: "Web" }
+]));
+console.log(organizeInstructors([
+  { name: "Brendan", course: "Blockchain" },
+  { name: "David", course: "Web" },
+  { name: "Martha", course: "iOS" },
+  { name: "Carlos", course: "Web" }
+]));
+
+
+
+
+
+
+
+const organizeInstructors = function (instructors) {
+  let classGuide = {
+    Blockchain: [],
+    iOS: [],
+    Web: []
+  };
+  for (let subject of instructors) {
+    if (subject.course === 'iOS') {
+      classGuide.iOS.push(subject.name)
+    } else if (subject.course === 'Web') {
+      classGuide.Web.push(subject.name)
+    } else if (subject.course === 'Blockchain') {
+      classGuide.Blockchain.push(subject.name)
+    }
+  }
+
+  return classGuide;
+}
+console.log(organizeInstructors([
+  { name: "Brendan", course: "Blockchain" },
+  { name: "David", course: "Web" },
+  { name: "Martha", course: "iOS" },
+  { name: "Carlos", course: "Web" }
+]));
