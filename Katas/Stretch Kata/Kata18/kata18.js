59f50bcff26d286ce9314de8f210bc62c99914fa
hansori01 <19sorihan88@gmail.com>
2021-01-25 02:04:51 +0000

    katas

--- Katas/Stretch Kata/Kata18/kata18.js
+++ Katas/Stretch Kata/Kata18/kata18.js
@@ -8,13 +8,28 @@ If answer is not a number print an error and don't count it as a try.
 
 - declare a function
 - random number generator
-
-
-
-
 */
 
-const prompt = require('prompt-sync')();
- 
-const name = prompt('What is your name?');
-console.log(`Hey there ${name}`);
+let randomNum = Math.floor(Math.random() * 100) + 1 //random number generator 
+let prompt = require("prompt-sync")(); // prompt-sync prompter
+let count = 0; // attempted try counter
+let correct = false;
+
+while (!correct) { //loops until guess matches the random number
+  let answer = prompt("Guess a number from 1 - 100: "); // asks for input and stores in answer
+  answer = Number(answer); // we make the answer a number
+  if (isNaN(answer)) { // isNaN - number checker
+    count++; //adds to the counter
+    console.log('ok.. this is not a number..')
+  } else if (answer === randomNum) { // if guessed correctly, we change the correct value to stop the loop
+    correct = true;
+    count++;
+    console.log(`You done did it!! It only took you ${count} attempts :)`)
+  } else if (answer < randomNum) { // if guess is lower than number
+    count++;
+    console.log('Think higher...')
+  } else if (answer > randomNum) {// if guess is higher than number
+    count++;
+    console.log('ooh you 2 high child');
+  }
+}
