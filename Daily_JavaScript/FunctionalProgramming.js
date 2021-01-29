a48a5ffb382bcd136803da7b74034d6c898e8cb4
hansori01 <19sorihan88@gmail.com>
2021-01-29 12:55:58 -0800

    functional programming

--- Daily_JavaScript/FunctionalProgramming.js
+++ Daily_JavaScript/FunctionalProgramming.js
@@ -9,3 +9,26 @@ const squareList = arr => {
   
   const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
   console.log(squaredIntegers);
+
+
+  //returning a sorted array without changing the original array
+  //*functional programming stays within scope and doesn't mutate outside variables
+
+  var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  return arr.slice().sort((a, b)=> a - b)
+}
+console.log(nonMutatingSort(globalArray));
+
+
+// currying using arrow function
+
+const add = x => y => z => x + y + z;
+console.log(add(1)(2)(3))
+
+// above uncurried would be:
+
+const add = (x,y,z) => x + y + z;
+console.log(add(1,2,3))
+
+//useful if you can't supply all the arguments to a function at one time.
